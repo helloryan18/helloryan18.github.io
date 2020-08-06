551051ef829b3c6f10f5cad7f0d4daa802ddda38
wfeng16 <wfeng16@ucsc.edu>
2020-08-06 05:36:22 -0700

    update

---
+++ js/edit.js
@@ -0,0 +1,22 @@
+function selectRemove() {
+	$("#select2").attr("class", "");
+	$("#select1").attr("class", "active");
+	$("#remove").show();
+	$("#add").hide();
+}
+
+function selectAdd() {
+	$("#select2").attr("class", "active");
+	$("#select1").attr("class", "");
+	$("#remove").hide();
+	$("#add").show();
+}
+
+function addItem() {
+	var node = document.createElement("div");
+	node.innerHTML = "newnewnew";
+	node.setAttribute("class", "col-lg-6 col-md-6 col-sm-6 col-xs-6");
+	node.setAttribute("style", "margin-left: auto; margin-right: auto;");
+
+	document.getElementById("info").appendChild(node);
+}
